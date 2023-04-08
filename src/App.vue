cc9a5f6b8b721206d17ac3e70d85ec1647135ad6
EndoHizumi <endohizumi0114@gmail.com>
2023-04-08 22:04:40 +0900

    とりあえず、笛はなる

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,52 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img id="image" src="./assets/logo.png" @click="start" :style="imgStyle" />
+    <audio id="se" :src="seSoruce" ></audio>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  data() {
+    return {
+      isDragging: false,
+      position: 0,
+      seSoruce: "./nc102252.wav"
+    };
+  },
+  computed: {
+    imgStyle() {
+      return {
+        right: this.position + "px",
+      };
+    },
+  },
+  methods: {
+    start: () => {
+      document.getElementById('se').play()
+    }
+  },
+  mounted() {
+    document.getElementById('se').addEventListener("ended", () => {
+      const x = 10;
+      if (this.isDragging) {
+        this.position -= x;
+      } else {
+        this.position += x;
+      }
+      document.getElementById("se").play()
+    })
+    document.addEventListener("mousedown", () => {
+      this.isDragging = true;
+    });
+    document.addEventListener("mouseup", () => {
+      this.isDragging = false;
+    });
+  },
+};
 </script>
+
 
 <style>
 #app {
@@ -24,5 +56,11 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+#image {
+  position: absolute;
+  right: 0;
+  top: 0;
 }
 </style>
